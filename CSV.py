431b9d87f9584af05aa15fdb32d9d3160f718719
unknown <raghavbhavyasingh@gmail.com>
2025-12-28 23:32:04 +0530

    assigning different datatypes to cols using dtypes as dictionary

--- CSV.py
+++ CSV.py
@@ -9,11 +9,11 @@ print(df.head())
 
 #converting string to a in-memory file format using StringIO
 
-data=('col1,col2,col3\n'
+data1=('col1,col2,col3\n'
       'x,y,1\n'
       'a,b,c')
 
-file_formated_data=StringIO(data)
+file_formated_data=StringIO(data1)
 df1=pd.read_csv(file_formated_data)
 print(df1)
 
@@ -24,3 +24,12 @@ print(pd.read_csv('mercedesbenz.csv',usecols=['X0','X1','X2','X3']))
 print(df1.to_csv())
 
 print(df1.to_csv(index=False)) # when dont want indexes to be converted
+
+# assigning diff dataypes to diff columns
+data2=('a,b,c,d\n'
+       '1,2,3,4\n'
+       '5,6,7,8\n'
+       '9,10,11')
+
+df2= pd.read_csv(StringIO(data2),dtype={'a':int,'b':float,'c':float})
+print(df2.info())
