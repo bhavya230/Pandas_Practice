75041d82bfc55bb002b6f8a383e1390d188b42d8
unknown <raghavbhavyasingh@gmail.com>
2026-01-08 16:58:41 +0530

    reading json data

---
+++ JSON.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from io import StringIO
+
+data='{"employee_name": "james" , "email" : "james@gmail.com","job_profile":[{"title1": "Team lead", "title2": "Sr.Developer"}]}'
+
+print(pd.read_json(StringIO(data)))
+
+
