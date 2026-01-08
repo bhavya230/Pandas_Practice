d955d3821d58b080549cfcf3b00e9d7723ff4e41
unknown <raghavbhavyasingh@gmail.com>
2026-01-08 21:30:17 +0530

    dataframe to json object

--- JSON.py
+++ JSON.py
@@ -12,3 +12,14 @@ print(pd.read_json(StringIO(data),orient='records'))
 print(pd.read_json(StringIO(data),orient='index'))
 #--split
 print(pd.read_json(StringIO(data),orient='split'))
+
+# DataFrame to JSON OBJECT 
+df=pd.DataFrame([['a','b'],['c','d']],index=['row1','row2'],columns=['col1','col2'])
+print(df)
+print(df.to_json())
+# combining orient attribute
+print(df.to_json(orient='index'))
+print(df.to_json(orient='columns'))
+print(df.to_json(orient='records'))
+print(df.to_json(orient='split'))
+print(df.to_json(orient='values'))
