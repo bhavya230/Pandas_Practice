a03f469563d560010f720061ca5c531b862b299c
unknown <raghavbhavyasingh@gmail.com>
2026-01-08 22:47:37 +0530

    json normalization

--- JSON.py
+++ JSON.py
@@ -23,3 +23,31 @@ print(df.to_json(orient='columns'))
 print(df.to_json(orient='records'))
 print(df.to_json(orient='split'))
 print(df.to_json(orient='values'))
+
+# JSON NORMALIZATION
+data=[
+  {
+    "employee_name": "james",
+    "email": "james@gmail.com",
+    "job_profile": {
+      "title1": "Team lead",
+      "title2": "Sr.Developer"
+    }
+  }
+]
+print(pd.json_normalize(data))
+
+data=[
+    {         
+        "id": 1,
+        "name": "Cole Volk",
+        "fitness": {"height": 130, "weight": 60},
+    },
+    {"name": "Mark Reg", "fitness": {"height": 130, "weight": 60}},
+    {
+        "id": 2,
+        "name": "Faye Raker",
+        "fitness": {"height": 130, "weight": 60},
+    },
+]
+print(pd.json_normalize(data))
