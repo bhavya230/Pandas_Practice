05a7db9199e60da70ab05dc31ca9b6e2ce28f8fd
unknown <raghavbhavyasingh@gmail.com>
2026-01-14 12:09:38 +0530

    match parameter

--- HTML.py
+++ HTML.py
@@ -7,3 +7,7 @@ print(html) # prints list of all tables
 
 print(html[0]) # get 1st table 
 type(html[0]) # each table would be a DATAFRAME
+
+# match parameter -- to get specific table
+html=pd.read_html("https://en.wikipedia.org/wiki/Mobile_country_code",match="Government debt")
+print(html[0])
