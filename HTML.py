92c1cc8561b601e5097dd0585ea6bb73c7c975b3
unknown <raghavbhavyasingh@gmail.com>
2026-01-14 12:10:53 +0530

    converting back to html

--- HTML.py
+++ HTML.py
@@ -11,3 +11,6 @@ type(html[0]) # each table would be a DATAFRAME
 # match parameter -- to get specific table
 html=pd.read_html("https://en.wikipedia.org/wiki/Mobile_country_code",match="Government debt")
 print(html[0])
+
+# converting back to html
+html[0].to_html('demo.html')
