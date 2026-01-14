d6415a10995b692532553063cdefa5e022ea890e
unknown <raghavbhavyasingh@gmail.com>
2026-01-14 12:04:13 +0530

    reading html file

--- HTML.py
+++ HTML.py
@@ -1,4 +1,9 @@
 # HOW TO READ HTML FILES WITH THE HELP OF PANDAS 
 import pandas as pd
 
-html=pd.read_html("https://en.wikipedia.org/wiki/Mobile_country_code")
+html=pd.read_html("https://en.wikipedia.org/wiki/Mobile_country_code") # reads tables from html pages
+print(type(html)) # list 
+print(html) # prints list of all tables 
+
+print(html[0]) # get 1st table 
+type(html[0]) # each table would be a DATAFRAME
